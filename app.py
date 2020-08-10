fbe7beaef2f5788edbe3697a6980d552ae39967e
Anna Spencer <annaspencerdev@gmail.com>
2020-08-09 19:20:46 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+from stories import story
+app = Flask(__name__)
+
+@app.route("/")
+def show_form():
+    prompts = story.prompts
+
+    return render_template("form.html", prompts=prompts)
+
+@app.route("/story")
+def make_story():
+    text = story.generate(request.args)
+
+    return render_template("story.html", text=text)
